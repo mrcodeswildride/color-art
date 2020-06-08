19351f4152f26876aeedd1e0201d1f31cbf0e7bc
David Keller <mrcodeswildride@gmail.com>
2020-06-08 13:23:48 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,89 +1,75 @@
-var square1 = document.getElementById("square1");
-var square2 = document.getElementById("square2");
-var square3 = document.getElementById("square3");
-var square4 = document.getElementById("square4");
-var square5 = document.getElementById("square5");
-var square6 = document.getElementById("square6");
-var square7 = document.getElementById("square7");
-var square8 = document.getElementById("square8");
-var square9 = document.getElementById("square9");
+let square1 = document.getElementById(`square1`)
+let square2 = document.getElementById(`square2`)
+let square3 = document.getElementById(`square3`)
+let square4 = document.getElementById(`square4`)
+let square5 = document.getElementById(`square5`)
+let square6 = document.getElementById(`square6`)
+let square7 = document.getElementById(`square7`)
+let square8 = document.getElementById(`square8`)
+let square9 = document.getElementById(`square9`)
 
-var noBackgroundButton = document.getElementById("noBackground");
-var redButton = document.getElementById("red");
-var greenButton = document.getElementById("green");
-var blueButton = document.getElementById("blue");
-var selectedBackgroundParagraph = document.getElementById("selectedBackground");
+let redButton = document.getElementById(`redButton`)
+let greenButton = document.getElementById(`greenButton`)
+let blueButton = document.getElementById(`blueButton`)
+let colorParagraph = document.getElementById(`colorParagraph`)
 
-var opacity25Button = document.getElementById("opacity25");
-var opacity50Button = document.getElementById("opacity50");
-var opacity75Button = document.getElementById("opacity75");
-var opacity100Button = document.getElementById("opacity100");
-var selectedOpacityParagraph = document.getElementById("selectedOpacity");
+let opacity25Button = document.getElementById(`opacity25Button`)
+let opacity50Button = document.getElementById(`opacity50Button`)
+let opacity750Button = document.getElementById(`opacity75Button`)
+let opacityParagraph = document.getElementById(`opacityParagraph`)
 
-var selectedBackground = "";
-var selectedOpacity = "";
+let selectedColor = ``
+let selectedOpacity = ``
 
-square1.addEventListener("click", selectSquare);
-square2.addEventListener("click", selectSquare);
-square3.addEventListener("click", selectSquare);
-square4.addEventListener("click", selectSquare);
-square5.addEventListener("click", selectSquare);
-square6.addEventListener("click", selectSquare);
-square7.addEventListener("click", selectSquare);
-square8.addEventListener("click", selectSquare);
-square9.addEventListener("click", selectSquare);
+square1.addEventListener(`click`, changeSquare)
+square2.addEventListener(`click`, changeSquare)
+square3.addEventListener(`click`, changeSquare)
+square4.addEventListener(`click`, changeSquare)
+square5.addEventListener(`click`, changeSquare)
+square6.addEventListener(`click`, changeSquare)
+square7.addEventListener(`click`, changeSquare)
+square8.addEventListener(`click`, changeSquare)
+square9.addEventListener(`click`, changeSquare)
 
-noBackgroundButton.addEventListener("click", setNoBackground);
-redButton.addEventListener("click", setRed);
-greenButton.addEventListener("click", setGreen);
-blueButton.addEventListener("click", setBlue);
+redButton.addEventListener(`click`, selectRed)
+greenButton.addEventListener(`click`, selectGreen)
+blueButton.addEventListener(`click`, selectBlue)
 
-opacity25Button.addEventListener("click", setOpacity25);
-opacity50Button.addEventListener("click", setOpacity50);
-opacity75Button.addEventListener("click", setOpacity75);
-opacity100Button.addEventListener("click", setOpacity100);
+opacity25Button.addEventListener(`click`, select25)
+opacity50Button.addEventListener(`click`, select50)
+opacity75Button.addEventListener(`click`, select75)
 
-function selectSquare() {
-    this.style.backgroundColor = selectedBackground;
-    this.style.opacity = selectedOpacity;
+function changeSquare() {
+  this.style.backgroundColor = selectedColor
+  this.style.opacity = selectedOpacity
 }
 
-function setNoBackground() {
-    selectedBackground = "";
-    selectedBackgroundParagraph.innerHTML = "Selected color: none";
+function selectRed() {
+  selectedColor = `red`
+  colorParagraph.innerHTML = `Selected color: red`
 }
 
-function setRed() {
-    selectedBackground = "red";
-    selectedBackgroundParagraph.innerHTML = "Selected color: red";
+function selectGreen() {
+  selectedColor = `green`
+  colorParagraph.innerHTML = `Selected color: green`
 }
 
-function setGreen() {
-    selectedBackground = "green";
-    selectedBackgroundParagraph.innerHTML = "Selected color: green";
+function selectBlue() {
+  selectedColor = `blue`
+  colorParagraph.innerHTML = `Selected color: blue`
 }
 
-function setBlue() {
-    selectedBackground = "blue";
-    selectedBackgroundParagraph.innerHTML = "Selected color: blue";
+function select25() {
+  selectedOpacity = `25%`
+  opacityParagraph.innerHTML = `Selected opacity: 25%`
 }
 
-function setOpacity25() {
-    selectedOpacity = 0.25;
-    selectedOpacityParagraph.innerHTML = "Selected opacity: 25%";
+function select50() {
+  selectedOpacity = `50%`
+  opacityParagraph.innerHTML = `Selected opacity: 50%`
 }
 
-function setOpacity50() {
-    selectedOpacity = 0.5;
-    selectedOpacityParagraph.innerHTML = "Selected opacity: 50%";
-}
-
-function setOpacity75() {
-    selectedOpacity = 0.75;
-    selectedOpacityParagraph.innerHTML = "Selected opacity: 75%";
-}
-
-function setOpacity100() {
-    selectedOpacity = 1;
-    selectedOpacityParagraph.innerHTML = "Selected opacity: 100%";
+function select75() {
+  selectedOpacity = `75%`
+  opacityParagraph.innerHTML = `Selected opacity: 75%`
 }
